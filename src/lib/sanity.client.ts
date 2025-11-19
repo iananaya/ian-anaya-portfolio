db9cd7bdbb612b33df38e6e7c4b6dc2b92253e0f
iananaya <2brosmakinacardgame@gmail.com>
2025-11-19 08:48:12 -0600

    Use env variables for Sanity client

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -2,8 +2,10 @@
 import { createClient } from '@sanity/client';
 
 export const sanity = createClient({
-  projectId: '7lj6cjs8',
-  dataset: 'production',
-  apiVersion: '2023-01-01',
-  useCdn: true,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '7lj6cjs8',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  apiVersion: process.env.SANITY_API_VERSION || '2023-01-01',
+  useCdn: true, // `true` = faster, cached reads
+  token: process.env.SANITY_READ_TOKEN, // optional: only needed if dataset is private
 });
+
